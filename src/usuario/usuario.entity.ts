de2c15af002028b03879fcea90f5cf7a7459509e
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-12-18 18:07:09 -0300

    Manipulando dados com pipes

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,8 +1,28 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class Usuario {
     id: number;
+
+    @IsNotEmpty({
+        message: 'nomeDeUsuario é obrigatório'
+    })
+    @IsString({
+        message: 'nomeDeUsuario deve ser string'
+    })
     nomeDeUsuario: string;
+
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty({
+        message: 'senha é obrigatória'
+    })
     senha: string;
+
+    @IsNotEmpty({
+        message: 'nomeCompleto é obrigatório'
+    })
     nomeCompleto: string;
+
     dataDeEntrada: Date;
 }
